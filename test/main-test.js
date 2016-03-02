f3751df482a1564f900d62af25ec7c7e243df35a
cai4560 <495781086@qq.com>
2016-03-02 15:50:42 +0800

    Added jasmine and work files

---
+++ test/main-test.js
@@ -0,0 +1,6 @@
+describe('Main-Test: Spell out a number', function() {
+	describe('It Should Spell out correct info', function() {
+		it('when given a 8-bits input', function() {
+		});
+	});
+});
---
+++ test/task-test.js
@@ -0,0 +1,6 @@
+describe('Task 1: GetSplitArrs', function() {
+	describe('It should return correct splitArrs',function() {
+		it('when given a 8-bits input', function() {
+		});
+	});
+});
