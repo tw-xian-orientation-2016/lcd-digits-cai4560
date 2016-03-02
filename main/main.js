e1f1d9bc4f74402e71f3e5c16c730e24a371ce88
cai4560 <495781086@qq.com>
2016-03-02 15:58:52 +0800

    Finished the 1st task

--- main/main.js
+++ main/main.js
@@ -1,1 +1,10 @@
 /*     Task 1     */
+function getDividedNumArr(number) {
+	var dividedNumArr = [];
+	for (var i = 0; i < number.length; i++) {
+		dividedNumArr.push(parseInt(number[i]));
+	}
+	return dividedNumArr;
+}
+
+/*     Task 2     */
--- test/task-test.js
+++ test/task-test.js
@@ -1,6 +1,10 @@
-describe('Task 1: GetSplitArrs', function() {
-	describe('It should return correct splitArrs',function() {
-		it('when given a 8-bits input', function() {
+describe('Task 1: GetDividedtArr', function() {
+	describe('It should return correct DividedtArr',function() {
+		it('when given a 10-bits input', function() {
+			var input = "9876543210";
+			var expectResult = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+			var result = getDividedNumArr(input);
+			expect(result).toEqual(expectResult) ;
 		});
 	});
 });
